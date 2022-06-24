d4c7bf41144a55e2eb053467f85b29c2c3c7b1ec
Saloni <saloni.asrani10@gmail.com>
2022-06-24 15:31:34 +0530

    DARK

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 function toggleDropdown() {
-    let navbarToggle = document.getElementById("navbar-toggle");
-    if (navbarToggle.className === 'topnav') {
-      navbarToggle.className += ' responsive';
-    } else {
-      navbarToggle.className = 'topnav';
-    }
+  let navbarToggle = document.getElementById("navbar-toggle");
+  if (navbarToggle.className === "topnav") {
+    navbarToggle.className += " responsive";
+  } else {
+    navbarToggle.className = "topnav";
   }
+}
+
+// document.getElementById("navbar-toggle").addEventListener(
+//   "click",
+//   (e) => {
+//     if (e.target.id) {
+//       window.location.href = e.target.id + ".html";
+//     }
+//     e.stopPropagation();
+//   },
+//   true
+// );
